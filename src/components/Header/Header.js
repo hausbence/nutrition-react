68594d527aed49aec42d8294f0557ca471cf0024
elekadam21 <elekadam21@gmail.com>
2021-02-25 23:57:32 +0100

    fixed an error in ingredients where items could have the same keys, added function if you click outside of menu that closes it, and some fixes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { useHistory } from "react-router-dom";
 import { StyledHeader } from "./Header.styled";
 import { Burger, Menu } from "../../components";
+import { useOnClickOutside } from "../../hooks/useOnClickOutside";
+import FocusLock from "react-focus-lock";
 
 const Header = () => {
 	let history = useHistory();
 	const [open, setOpen] = useState(false);
+	const node = useRef();
+	const menuId = "main-menu";
+
+	useOnClickOutside(node, () => setOpen(false));
 
 	const handleKeyPress = (event) => {
 		if (event.key === "Enter") {
@@ -18,8 +24,12 @@ const Header = () => {
 	return (
 		<StyledHeader>
 			<h1>nutri-core</h1>
-			<Burger open={open} setOpen={setOpen} />
-			<Menu open={open} setOpen={setOpen} />
+			<div ref={node}>
+				<FocusLock disabled={!open}>
+					<Burger open={open} setOpen={setOpen} aria-controls={menuId} />
+					<Menu open={open} setOpen={setOpen} id={menuId} />
+				</FocusLock>
+			</div>
 			<input
 				type="text"
 				placeholder="Search recipes..."
