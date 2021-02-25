b5aa90161912f4588c55a971ce8f9c78776e1bc6
elekadam21 <elekadam21@gmail.com>
2021-02-26 00:28:29 +0100

    started reworking registration form

--- src/Pages/Account/Register.js
+++ src/Pages/Account/Register.js
@@ -1,168 +1,48 @@
 import React, { useState, useRef } from "react";
-import Form from "react-validation/build/form";
-import Input from "react-validation/build/input";
-import CheckButton from "react-validation/build/button";
-import { isEmail } from "validator";
+import { useForm } from "react-hook-form";
 
 import AuthService from "../services/auth.service";
 
-const required = (value) => {
-	if (!value) {
-		return (
-			<div className="alert alert-danger" role="alert">
-				This field is required!
-			</div>
-		);
-	}
-};
-
-const validEmail = (value) => {
-	if (!isEmail(value)) {
-		return (
-			<div className="alert alert-danger" role="alert">
-				This is not a valid email.
-			</div>
-		);
-	}
-};
-
-const vusername = (value) => {
-	if (value.length < 3 || value.length > 20) {
-		return (
-			<div className="alert alert-danger" role="alert">
-				The username must be between 3 and 20 characters.
-			</div>
-		);
-	}
-};
-
-const vpassword = (value) => {
-	if (value.length < 6 || value.length > 40) {
-		return (
-			<div className="alert alert-danger" role="alert">
-				The password must be between 6 and 40 characters.
-			</div>
-		);
-	}
-};
-
 const Register = (props) => {
-	const form = useRef();
-	const checkBtn = useRef();
-
-	const [username, setUsername] = useState("");
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
-	const [successful, setSuccessful] = useState(false);
-	const [message, setMessage] = useState("");
-
-	const onChangeUsername = (e) => {
-		const username = e.target.value;
-		setUsername(username);
-	};
-
-	const onChangeEmail = (e) => {
-		const email = e.target.value;
-		setEmail(email);
-	};
-
-	const onChangePassword = (e) => {
-		const password = e.target.value;
-		setPassword(password);
-	};
+	const { register, handleSubmit } = useForm();
 
 	const handleRegister = (e) => {
 		e.preventDefault();
+		console.log(e);
+		// AuthService.register(username, email, password).then(
+		// 	(response) => {
+		// 		setMessage(response.data.message);
+		// 		setSuccessful(true);
+		// 	},
+		// 	(error) => {
+		// 		const resMessage =
+		// 			(error.response &&
+		// 				error.response.data &&
+		// 				error.response.data.message) ||
+		// 			error.message ||
+		// 			error.toString();
 
-		setMessage("");
-		setSuccessful(false);
-
-		form.current.validateAll();
-
-		if (checkBtn.current.context._errors.length === 0) {
-			AuthService.register(username, email, password).then(
-				(response) => {
-					setMessage(response.data.message);
-					setSuccessful(true);
-				},
-				(error) => {
-					const resMessage =
-						(error.response &&
-							error.response.data &&
-							error.response.data.message) ||
-						error.message ||
-						error.toString();
-
-					setMessage(resMessage);
-					setSuccessful(false);
-				}
-			);
-		}
+		// 		setMessage(resMessage);
+		// 		setSuccessful(false);
+		// 	}
+		// );
 	};
 
 	return (
 		<div className="form">
-			<h2 className="loginHeader">Registration</h2>
-			<Form onSubmit={handleRegister} ref={form}>
-				{!successful && (
-					<div className="input-fields">
-						<div className="field">
-							<label htmlFor="username">Username</label>
-							<Input
-								type="text"
-								className="input-style"
-								name="username"
-								value={username}
-								placeholder="Username"
-								onChange={onChangeUsername}
-								validations={[required, vusername]}
-							/>
-						</div>
-
-						<div className="field">
-							<label htmlFor="email">Email</label>
-							<Input
-								type="text"
-								className="input-style"
-								name="email"
-								value={email}
-								placeholder="Email"
-								onChange={onChangeEmail}
-								validations={[required, validEmail]}
-							/>
-						</div>
-
-						<div className="field">
-							<label htmlFor="password">Password</label>
-							<Input
-								type="password"
-								className="input-style"
-								name="password"
-								value={password}
-								placeholder="Password"
-								onChange={onChangePassword}
-								validations={[required, vpassword]}
-							/>
-						</div>
-
-						<button className="button">Sign Up</button>
-					</div>
-				)}
-
-				{message && (
-					<div className="form">
-						<div
-							className={
-								successful ? "alert alert-success" : "alert alert-danger"
-							}
-							role="alert"
-						>
-							{message}
-						</div>
-					</div>
-				)}
-				<CheckButton style={{ display: "none" }} ref={checkBtn} />
-			</Form>
+			<br />
+			<br />
+			<br />
+			<br />
+			<form onSubmit={handleSubmit(handleRegister)}>
+				<input
+					name="firstName"
+					ref={register({ required: true, maxLength: 20 })}
+				/>
+				<input name="lastName" ref={register({ pattern: /^[A-Za-z]+$/i })} />
+				<input name="age" type="number" ref={register({ min: 18, max: 99 })} />
+				<input type="submit" />
+			</form>
 		</div>
 	);
 };
