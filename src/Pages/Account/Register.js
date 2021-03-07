971dfdc6c523c4e4a260f4fe50576b8054690729
elekadam21 <elekadam21@gmail.com>
2021-03-07 19:13:45 +0100

    fixed registration form

--- src/Pages/Account/Register.js
+++ src/Pages/Account/Register.js
@@ -5,27 +5,48 @@ import AuthService from "../services/auth.service";
 
 const Register = (props) => {
 	const { register, handleSubmit } = useForm();
+	const [username, setUsername] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [successful, setSuccessful] = useState(false);
+	const [message, setMessage] = useState("");
+
+	const onChangeUsername = (e) => {
+		const username = e.target.value;
+		setUsername(username);
+	};
+
+	const onChangeEmail = (e) => {
+		const email = e.target.value;
+		setEmail(email);
+	};
+
+	const onChangePassword = (e) => {
+		const password = e.target.value;
+		setPassword(password);
+	};
 
 	const handleRegister = (e) => {
-		e.preventDefault();
-		console.log(e);
-		// AuthService.register(username, email, password).then(
-		// 	(response) => {
-		// 		setMessage(response.data.message);
-		// 		setSuccessful(true);
-		// 	},
-		// 	(error) => {
-		// 		const resMessage =
-		// 			(error.response &&
-		// 				error.response.data &&
-		// 				error.response.data.message) ||
-		// 			error.message ||
-		// 			error.toString();
+		setMessage("");
+		setSuccessful(false);
 
-		// 		setMessage(resMessage);
-		// 		setSuccessful(false);
-		// 	}
-		// );
+		AuthService.register(username, email, password).then(
+			(response) => {
+				setMessage(response.data.message);
+				setSuccessful(true);
+			},
+			(error) => {
+				const resMessage =
+					(error.response &&
+						error.response.data &&
+						error.response.data.message) ||
+					error.message ||
+					error.toString();
+
+				setMessage(resMessage);
+				setSuccessful(false);
+			}
+		);
 	};
 
 	return (
@@ -34,15 +55,53 @@ const Register = (props) => {
 			<br />
 			<br />
 			<br />
+			<h2 className="loginHeader">Registration</h2>
 			<form onSubmit={handleSubmit(handleRegister)}>
-				<input
-					name="firstName"
-					ref={register({ required: true, maxLength: 20 })}
-				/>
-				<input name="lastName" ref={register({ pattern: /^[A-Za-z]+$/i })} />
-				<input name="age" type="number" ref={register({ min: 18, max: 99 })} />
+				<div className="input-fields">
+					<div className="field">
+						<label htmlFor="username">Username</label>
+						<input
+							type="text"
+							className="input-style"
+							name="username"
+							value={username}
+							placeholder="Username"
+							onChange={onChangeUsername}
+							required
+							minLength="4"
+							maxLength="19"
+						/>
+					</div>
+					<div className="field">
+						<label htmlFor="email">Email</label>
+						<input
+							type="email"
+							className="input-style"
+							name="email"
+							value={email}
+							placeholder="Email"
+							onChange={onChangeEmail}
+							required
+						/>
+					</div>
+					<div className="field">
+						<label htmlFor="password">Password</label>
+						<input
+							type="password"
+							className="input-style"
+							name="password"
+							value={password}
+							placeholder="Password"
+							onChange={onChangePassword}
+							required
+							minLength="6"
+							maxLength="40"
+						/>
+					</div>
+				</div>
 				<input type="submit" />
 			</form>
+			{message}
 		</div>
 	);
 };
