ddd47f787b082546edb4557be063c43d59225dee
elekadam21 <elekadam21@gmail.com>
2021-03-10 16:40:54 +0100

    added a feature where the user can select from all the different diets and can remove them, no duplicates, fully working

--- src/Pages/mealplanner/MealPlannerPage.js
+++ src/Pages/mealplanner/MealPlannerPage.js
@@ -15,10 +15,37 @@ const MealPlannerPage = () => {
 		"Primal",
 		"Whole30",
 	];
+	const [selectedDiets, setSelectedDiets] = useState([]);
 
 	const onChangeCalories = (e) => {
 		const calories = e.target.value;
 		setCalories(calories);
+	};
+
+	const onAddButtonClick = (e) => {
+		e.preventDefault();
+		const diet = e.target.value;
+		addToSelectedDiets(diet);
+	};
+
+	const addToSelectedDiets = (dietToAdd) => {
+		if (selectedDiets.includes(dietToAdd)) {
+			removeFromSelectedDiets(dietToAdd);
+		} else {
+			const newDietArray = [...selectedDiets, dietToAdd];
+			setSelectedDiets(newDietArray);
+		}
+	};
+
+	const onRemoveButtonClicK = (e) => {
+		e.preventDefault();
+		const dietToRemove = e.target.value;
+		removeFromSelectedDiets(dietToRemove);
+	};
+
+	const removeFromSelectedDiets = (dietToRemove) => {
+		const newDietArray = selectedDiets.filter((diet) => diet !== dietToRemove);
+		setSelectedDiets(newDietArray);
 	};
 
 	return (
@@ -37,10 +64,23 @@ const MealPlannerPage = () => {
 						required
 						max="10000"
 					/>
-					<label for="diets">Choose diet(s):</label>
-					<select name="diets" size="1">
-						<option value="volvo">Volvo</option>
-					</select>
+					<div className="diets-selector">
+						{dietList.map((diet, i) => (
+							<button value={diet} key={i} onClick={onAddButtonClick}>
+								{diet}
+							</button>
+						))}
+					</div>
+					<div className="selected-diets">
+						{selectedDiets.map((diet, i) => (
+							<div key={i}>
+								{diet}
+								<button value={diet} onClick={onRemoveButtonClicK}>
+									X
+								</button>
+							</div>
+						))}
+					</div>
 				</form>
 			</StyledMealPlannerPage>
 		</React.Fragment>
